7cc8cef07b80f965d26ad3006fa0820bf608e873
Alessio Milanese <milanese.alessio@gmail.com>
2019-03-04 17:58:31 +0100

    add test for samtools that return code is 0

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ import os
 import sys
 import tempfile
 import subprocess
+import shlex
 
 # position of the script -------------------------------------------------------
 path_mOTUs = os.path.realpath(__file__)
@@ -25,6 +26,22 @@ relative_path = relative_path + "/"
 # ------------------------------------------------------------------------------
 # function to check if a specific tool exists
 # ------------------------------------------------------------------------------
+def is_tool_and_return0(name):
+    try:
+        devnull = open(os.devnull)
+        popenCMD = shlex.split(name)
+        child = subprocess.Popen(popenCMD, stdout=devnull, stderr=devnull)
+        streamdata = child.communicate()
+        rc = child.wait()
+        if rc == 0:
+            return True
+        else:
+            return False
+    except OSError as e:
+        if e.errno == os.errno.ENOENT:
+            return False
+    return True
+
 def is_tool(name):
     try:
         devnull = open(os.devnull)
@@ -90,8 +107,10 @@ def main(argv=None):
     # check samtools -----------------------------------------------------------
     sys.stderr.write("- samtools: ")
     if is_tool("samtools"):
-        sys.stderr.write(" correct\n")
-        #TODO: maybe check version? at least 1.5
+        if is_tool_and_return0("samtools --version"):
+            sys.stderr.write(" correct\n")
+        else:
+            sys.stderr.write(" WARNING. Samtools is not correctly installed\n")
     else:
         sys.stderr.write(" WARNING. Samtools is not in the path\n\n")
 
