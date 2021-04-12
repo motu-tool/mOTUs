065b9b190a8c8be4d3baedb5594107bd6ddf2ded
Alessio Milanese <milanese.alessio@gmail.com>
2021-04-12 18:52:23 +0200

    change to version 2.6.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 #
 # ============================================================================ #
 
-motus_version = "2.6.0"
+motus_version = "2.6.1"
 link_db = "https://zenodo.org/record/4588372/files/db_mOTU_v2.6.0.tar.gz"
 md5_db = "95dc2861cef5578a597a4739ee951fc2"
 DOI_db = "10.5281/zenodo.4588372"
